54268d205be98c5f9814006ac934d7eb3eada287
kohta  <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-09-19 16:51:39 +0900

    useRefを利用して文字列を取得

--- react-todo-app/src/App.js
+++ react-todo-app/src/App.js
@@ -1,15 +1,21 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import TodoList from "./TodoList";
 
 function App() {
 
   const [todos, setTodos ] = useState([{id: 1, name: "Todo1", completed:  false}]);
 
+  const todoNameRef = useRef();
+  const handleAddTodo = () => {
+    //タスクを追加する
+
+  };
+
   return (
     <div>
         <TodoList todos = {todos} />
-        <input tyoe="text"></input>
-        <button>タスクを追加</button>
+        <input tyoe="text" ref={todoNameRef}></input>
+        <button onClick={handleAddTodo}>タスクを追加</button>
         <button>完了したタスクの削除</button>
         <div>残りのタスクは : 0</div>
     </div>
