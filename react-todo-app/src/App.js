0f78c3f57ea33a2bcacbc0f1e327e04d2e772346
kohta  <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-09-19 16:15:42 +0900

    無駄な再レンダリングを防ぐ記述　追加

--- react-todo-app/src/App.js
+++ react-todo-app/src/App.js
@@ -1,9 +1,16 @@
 import TodoList from "./TodoList";
 
 function App() {
+  
+  const[ todos, setTodos ] = useState();
+
   return (
     <div>
         <TodoList />
+        <input tyoe="text"></input>
+        <button>タスクを追加</button>
+        <button>完了したタスクの削除</button>
+        <div>残りのタスクは : 0</div>
     </div>
   );
 }
