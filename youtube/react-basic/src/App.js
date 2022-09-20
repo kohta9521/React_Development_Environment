83711a22598e6b9853ed4557ad79951beb9e088c
kohta  <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-09-20 15:39:06 +0900

    コンポーネントの再利用

--- youtube/react-basic/src/App.js
+++ youtube/react-basic/src/App.js
@@ -7,7 +7,19 @@ function App() {
     <div>
         <Article
             title={'youtube教材 React 入門編'}
-            content={'今日のトピックはpropsについて!!(データの受け渡しについても追加)'}
+            content='今日のトピックはpropsについて!!(データの受け渡しについても追加)'
+        />
+                <Article
+            title={'youtube教材 React 初級編'}
+            content='今日のトピックはpropsについて!!(データの受け渡しについても追加)'
+        />
+                <Article
+            title={'youtube教材 React 中級編'}
+            content='今日のトピックはpropsについて!!(データの受け渡しについても追加)'
+        />
+                <Article
+            title={'youtube教材 React 上級編'}
+            content='今日のトピックはpropsについて!!(データの受け渡しについても追加)'
         />
     </div>
   );
