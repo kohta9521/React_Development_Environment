9449c39c68a6d4fecad3b14b112edef829faa6a3
kohta  <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-10-28 05:35:27 +0900

    props

--- react-tutorial-todo/src/App.js
+++ react-tutorial-todo/src/App.js
@@ -1,9 +1,12 @@
+import {useState} from "react";
 import TodoList from"./TodoList";
 
 function App() {
+  const [todos, setTodos] = useState(["Todo1", "Todo2"]);
+
   return (
     <>
-      <TodoList />
+      <TodoList todos={todos} />
       <input type="text" />
       <button>タスクを追加</button>
       <button>完了したタスクの削除</button>
