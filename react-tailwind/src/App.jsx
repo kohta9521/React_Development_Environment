e3e394ce5bf63a33ac14f677b77ce7a4ea1f3554
Kohta9521 <kohta0421kochi@gmail.com>
2022-11-20 22:18:53 +0900

    copy section

--- react-tailwind/src/App.jsx
+++ react-tailwind/src/App.jsx
@@ -184,8 +184,148 @@ function App() {
       </div>
     </section>
 
-    <section>
-      
+    <section className='text-gray-700 border-t border-gray-200'>
+      <div className='container px-5 py-24 mx-auto'>
+        <div className='text-center mb-20'>
+          <h1 className='text-2xl sm:text-3xl font-medium mb-2 text-gray-900'>About me</h1>
+          <p className='pb-10'>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit recusandae, eius tempora quae rerum debitis blanditiis obcaecati doloremque ex repudiandae?
+          </p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error consequuntur blanditiis earum expedita rerum, quia hic neque. Ratione soluta error ullam dolor
+            bus temporibus illum modi, corporis iusto aliquam eveniet incidunt voluptatibus minima quaerat quidem repudiandae itaque quisquam consequatur vero neces
+            sitatibus dolore. Iusto eligendi, libero minus perspiciatis officia unde saepe. Adipisci, harum ex dolorem laudantium vitae, sequi aperiam soluta dolor illo consec
+            tetur sed alias necessitatibus? Et voluptatum ratione magnam enim! Voluptas inventore itaque nemo praesentium debitis autem vitae a iure minus delectus ipsu
+            m dolorem dolor perferendis doloribus enim eum, iusto aut exercitationem iste deserunt doloremque officiis ullam odio? Quaerat, ipsum tenetur?
+          </p>
+        </div>
+{/* カードのdivタグ */}
+        <div className='flex flex-wrap'>
+          <div className='md:w-1/3 p-4'>
+            <div className='bg-gray-200 rounded-lg p-8'>
+              <div className='flex items-center mb-3'>
+                <div className='bg-green-500 text-white rounded-full'>
+                <svg
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M9 21.5L17.5 13L13 10L15 2.5L6.5 11L11 14L9 21.5Z" fill="currentColor" />
+                  </svg>
+                </div>
+                <h2 className='text-gray-900 text-lg font-medium ml-2'>
+                  Web Developer
+                </h2>
+              </div>
+              <div>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum non
+                  molestiae totam quidem aliquid assumenda dignissimos, optio minima sequi provident?
+                </p>
+                <a href="#" className='flex mt-3 text-green-500 items-center'>
+                  VIEW MORE
+                  <svg
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M15.0378 6.34317L13.6269 7.76069L16.8972 11.0157L3.29211 11.0293L3.29413 13.0293L16.8619 13.0157L13.6467 16.2459L15.0643 17.6568L20.7079 11.9868L15.0378 6.34317Z"
+                      fill="currentColor"
+                    />
+                  </svg>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className='md:w-1/3 p-4'>
+            <div className='bg-gray-200 rounded-lg p-8'>
+              <div className='flex items-center mb-3'>
+                <div className='bg-green-500 text-white rounded-full'>
+                <svg
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M9 21.5L17.5 13L13 10L15 2.5L6.5 11L11 14L9 21.5Z" fill="currentColor" />
+                  </svg>
+                </div>
+                <h2 className='text-gray-900 text-lg font-medium ml-2'>
+                  Web Developer
+                </h2>
+              </div>
+              <div>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum non
+                  molestiae totam quidem aliquid assumenda dignissimos, optio minima sequi provident?
+                </p>
+                <a href="#" className='flex mt-3 text-green-500 items-center'>
+                  VIEW MORE
+                  <svg
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M15.0378 6.34317L13.6269 7.76069L16.8972 11.0157L3.29211 11.0293L3.29413 13.0293L16.8619 13.0157L13.6467 16.2459L15.0643 17.6568L20.7079 11.9868L15.0378 6.34317Z"
+                      fill="currentColor"
+                    />
+                  </svg>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className='md:w-1/3 p-4'>
+            <div className='bg-gray-200 rounded-lg p-8'>
+              <div className='flex items-center mb-3'>
+                <div className='bg-green-500 text-white rounded-full'>
+                <svg
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M9 21.5L17.5 13L13 10L15 2.5L6.5 11L11 14L9 21.5Z" fill="currentColor" />
+                  </svg>
+                </div>
+                <h2 className='text-gray-900 text-lg font-medium ml-2'>
+                  Web Developer
+                </h2>
+              </div>
+              <div>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum non
+                  molestiae totam quidem aliquid assumenda dignissimos, optio minima sequi provident?
+                </p>
+                <a href="#" className='flex mt-3 text-green-500 items-center'>
+                  VIEW MORE
+                  <svg
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M15.0378 6.34317L13.6269 7.76069L16.8972 11.0157L3.29211 11.0293L3.29413 13.0293L16.8619 13.0157L13.6467 16.2459L15.0643 17.6568L20.7079 11.9868L15.0378 6.34317Z"
+                      fill="currentColor"
+                    />
+                  </svg>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
     </>
   )
