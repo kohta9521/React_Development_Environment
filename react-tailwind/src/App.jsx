eb806bb37b4a30e541a5f310eb13430466b78a3a
Kohta9521 <kohta0421kochi@gmail.com>
2022-11-20 20:32:04 +0900

    add app.jsx header html

--- react-tailwind/src/App.jsx
+++ react-tailwind/src/App.jsx
@@ -3,9 +3,21 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return (
+    <header className='text-gray-700'>
+      <div>
+        <a href="#">
+          <span>Kohta Code</span>
+        </a>
+        <nav>
+          <a href="#home">Home</a>
+          <a href="#about">About</a>
+          <a href="skills">Skills</a>
+          <a href="#blog">Blog</a>
+        </nav>
+      </div>
+    </header>
+  )
 }
 
 export default App
