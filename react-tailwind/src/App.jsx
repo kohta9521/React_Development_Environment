1de0dc86c36c12844f023ff1502884cc64745949
Kohta9521 <kohta0421kochi@gmail.com>
2022-11-20 20:49:12 +0900

    header complete

--- react-tailwind/src/App.jsx
+++ react-tailwind/src/App.jsx
@@ -5,15 +5,15 @@ import './App.css'
 function App() {
   return (
     <header className='text-gray-700 border-b border-gray-200'>
-      <div className='container flex'>
-        <a href="#">
-          <span>Kohta Code</span>
+      <div className='container flex mx-auto p-5 flex-col md:flex-row items-center'>
+        <a href="#" className='font-medium text-gray-900 mb-4 md:mb-0'>
+          <span className='text-xl ml-3'>Kohta Code</span>
         </a>
-        <nav>
-          <a href="#home">Home</a>
-          <a href="#about">About</a>
-          <a href="skills">Skills</a>
-          <a href="#blog">Blog</a>
+        <nav className='md:ml-auto text-base'>
+          <a href="#home" className='mr-5 hover:text-blue-400 duration-300'>Home</a>
+          <a href="#about" className='mr-5 hover:text-blue-400 duration-300'>About</a>
+          <a href="skills" className='mr-5 hover:text-blue-400 duration-300'>Skills</a>
+          <a href="#blog" className=' hover:text-blue-400 duration-300'>Blog</a>
         </nav>
       </div>
     </header>
