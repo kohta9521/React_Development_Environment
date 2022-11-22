7d73a32f126b3879efa9df6c823a85fa0f6e2fe0
Kohta9521 <kohta0421kochi@gmail.com>
2022-11-22 23:45:56 +0900

    stop this youtube video

--- react_portfoliosite/kohta_portfolio/src/App.jsx
+++ react_portfoliosite/kohta_portfolio/src/App.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import styles from './style';
 
 const App = () => (
     <div className='bg-primary w-full overflow-hidden'>
-      <div className=''>
-        
+      <div className={`${styles.paddingX} ${styles.flexcenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          Navbar
+        </div>
       </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+
+      </div>
+
     </div>
 );
 
