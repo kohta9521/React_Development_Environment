e5848665ac62fb016d017d0705095fa7bc954220
Kohta9521 <kohta0421kochi@gmail.com>
2022-11-25 17:31:46 +0900

    add const

--- udemy_basic/sec4/4_10/script.js
+++ udemy_basic/sec4/4_10/script.js
@@ -12,3 +12,22 @@ console.log(val1);
 // var変数は再宣言可能
 var val1 = "var変数を再宣言";
 console.log(val1);
+
+
+
+let val2 = "let変数";
+console.log(val2);
+
+//　letは上書きが可能
+val2 = "let変数を上書き";
+console.log(val2);
+
+// let は再宣言不可能
+// let val2 =  "let変数を再宣言";
+// console.log(val2);
+
+
+// 一番厳密な宣言方法
+
+const val3 = "const変数";
+console.log(val3);
