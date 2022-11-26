3609b79bf62fd1281c401e7213144d95d3d69327
Kohta9521 <kohta0421kochi@gmail.com>
2022-11-26 13:17:22 +0900

    add arrow func

---
+++ udemy_basic/sec4/4_21/script.js
@@ -0,0 +1,21 @@
+/**
+ * アロー関数
+ */
+
+// 従来の関数の画期かた
+function func1(str) {
+    return str
+}
+const func1 = function(str) {
+    return str;
+}
+
+console.log(func1("func1です。"));
+
+
+// アロー関数
+const func2 = (str) => {
+    return str;
+}
+
+console.log(func2("func2です。!!!"));
