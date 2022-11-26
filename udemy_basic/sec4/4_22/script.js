8b391a3f02fe19edf39e2a30f191f26350caad47
Kohta9521 <kohta0421kochi@gmail.com>
2022-11-26 14:50:27 +0900

    object solit

--- udemy_basic/sec4/4_22/script.js
+++ udemy_basic/sec4/4_22/script.js
@@ -9,3 +9,11 @@ const  myProfile = {
 
 const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
 console.log(message1);
+
+
+
+//  分割代入の書き方
+
+const { name, age } = myProfile;
+const message2 = `名前は${name}です。年齢は${age}歳です。`;
+console.log(message2);
