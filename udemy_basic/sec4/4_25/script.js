39605a38e146d64126377d211e02ff1c1a8c49d3
Kohta9521 <kohta0421kochi@gmail.com>
2022-11-26 23:17:28 +0900

    add map() filter()

--- udemy_basic/sec4/4_25/script.js
+++ udemy_basic/sec4/4_25/script.js
@@ -1,3 +1,29 @@
 /**
  * mapやfilterについて
  */
+
+
+const  nameArr = ["田中", "山田", "kohta"];
+
+for (let index = 0; index < nameArr.length; index ++) {
+    console.log(nameArr[index]);
+}
+
+
+const nameArr2 = nameArr.map((name) =>{
+    return name;
+})
+
+console.log(nameArr2);
+
+
+nameArr2.map((name) => console.log(name));
+
+
+
+// filterについて
+const numArr = [1, 2, 3, 4, 5];
+const newNumArr = numArr.filter((num) => {
+    return num % 2 === 1;
+});
+console.log(newNumArr);
