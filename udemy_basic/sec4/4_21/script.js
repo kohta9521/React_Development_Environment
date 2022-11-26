75c81fbec463d0c094efe4380c89fc4763949001
Kohta9521 <kohta0421kochi@gmail.com>
2022-11-26 14:40:46 +0900

    add short

--- udemy_basic/sec4/4_21/script.js
+++ udemy_basic/sec4/4_21/script.js
@@ -6,16 +6,14 @@
 function func1(str) {
     return str
 }
-const func1 = function(str) {
-    return str;
-}
+// const func1 = function(str) {
+//     return str;
+// }
 
 console.log(func1("func1です。"));
 
 
 // アロー関数
-const func2 = (str) => {
-    return str;
-}
+const func2 = str => str;
 
 console.log(func2("func2です。!!!"));
