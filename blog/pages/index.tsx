db2d43270fa2d02046513ebdb384e8832abcbd2e
Kohta9521 <kohta0421kochi@gmail.com>
2022-12-17 16:46:57 +0900

    add jsconfig.json

--- blog/pages/index.tsx
+++ blog/pages/index.tsx
@@ -3,9 +3,9 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 
-import Header from '../components/header';
-import Hero from '../components/hero';
-import Footer from '../components/footer';
+import Header from 'components/header';
+import Hero from 'components/hero';
+import Footer from 'components/footer';
 
 const inter = Inter({ subsets: ['latin'] })
 
