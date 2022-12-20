319de0831a59a340ccfe434dbc96dde21f315842
Kohta9521 <kohta0421kochi@gmail.com>
2022-12-20 23:41:33 +0900

    add all page Hero Props

--- nextjs/blog/pages/index.tsx
+++ nextjs/blog/pages/index.tsx
@@ -1,10 +1,12 @@
 import { Inter } from '@next/font/google'
 
-import Layout from '../components/layout'
 import Hero from '../components/hero'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return  <Hero />
+  return  <Hero 
+    title="CUBE"
+    subtitle="アウトプットしていくサイト"
+  />
 }
