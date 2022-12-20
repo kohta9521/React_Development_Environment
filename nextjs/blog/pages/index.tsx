034c547f817b073d0b0fa994e959151b2b34a1c9
Kohta9521 <kohta0421kochi@gmail.com>
2022-12-20 23:36:26 +0900

    add /blog page and /about page

--- nextjs/blog/pages/index.tsx
+++ nextjs/blog/pages/index.tsx
@@ -1,21 +1,10 @@
 import { Inter } from '@next/font/google'
 
-import Header from '../components/header'
+import Layout from '../components/layout'
 import Hero from '../components/hero'
-import Footer from '../components/footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return (
-    <>
-      <Header />
-
-      <main>
-        <Hero />
-      </main>
-
-      <Footer />
-    </>
-  )
+  return  <Hero />
 }
