ebe85b690b1e8d5fffb270fe158e6ad4e3dff73c
Kohta9521 <kohta0421kochi@gmail.com>
2022-12-20 23:16:36 +0900

    add conpoments and import index.tsx

--- nextjs/blog/pages/index.tsx
+++ nextjs/blog/pages/index.tsx
@@ -1,21 +1,21 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+
+import Header from '../components/header'
+import Hero from '../components/hero'
+import Footer from '../components/footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
-    <main>
-      <div>
-        <h1>CUBE</h1>
-        <p>アウトプットしていくサイト</p>
-      </div>
-    </main>
+      <Header />
 
-    <footer>FOOTER</footer>
+      <main>
+        <Hero />
+      </main>
+
+      <Footer />
     </>
   )
 }
