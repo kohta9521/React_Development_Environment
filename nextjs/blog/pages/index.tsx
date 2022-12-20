dc6effc947e122356ef77117f6b5809b9ac38985
Kohta9521 <kohta0421kochi@gmail.com>
2022-12-20 23:00:33 +0900

    add header and footer

--- nextjs/blog/pages/index.tsx
+++ nextjs/blog/pages/index.tsx
@@ -6,5 +6,18 @@ import styles from '../styles/Home.module.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return <h1>CUBE</h1>
+  return (
+    <>
+    <header>HEADER</header>
+
+    <main>
+      <div>
+        <h1>CUBE</h1>
+        <p>アウトプットしていくサイト</p>
+      </div>
+    </main>
+
+    <footer>FOOTER</footer>
+    </>
+  )
 }
