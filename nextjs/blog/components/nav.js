66d9ef96cb4ee34ce460600e59fb0a009265526a
Kohta9521 <kohta0421kochi@gmail.com>
2022-12-21 18:34:28 +0900

    add new nextjs app folider

--- nextjs/blog/components/nav.js
+++ nextjs/blog/components/nav.js
@@ -7,17 +7,17 @@ export default function Nav() {
             <ul>
                 <li>
                     <Link href="/">
-                        {/* <a id="link" className='link-item'>Home</a> */}
+                        <a id="link" className='link-item'>Home</a>
                     </Link>
                 </li>
                 <li>
                     <Link href="/about">
-                        {/* <a id="link"  className='link-item'>About</a> */}
+                        <a id="link"  className='link-item'>About</a>
                     </Link>
                 </li>
                 <li>
                     <Link href="/blog">
-                        {/* <a id="link" >Blog</a> */}
+                        <a id="link" >Blog</a>
                     </Link>
                 </li>
             </ul>
