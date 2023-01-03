b178ba761da72ced94b7f9a7aa27e6da4805e7fe
Kohta9521 <kohta0421kochi@gmail.com>
2023-01-03 17:40:43 +0900

    ページ制作の導入完了

--- re/blog/pages/index.js
+++ re/blog/pages/index.js
@@ -1,10 +1,20 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return <h1>CUBE</h1>
+  return (
+    <>
+      <header>HEADER</header>
+
+      <main>
+        <div>
+          <h1>CUBE</h1>
+          <p>アウトプットしていくサイト</p>
+        </div>
+      </main>
+
+      <fooer>FOOTER</fooer>
+    </>
+  )
 }
