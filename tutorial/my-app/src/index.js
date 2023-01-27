402e0fa665286d5a1ca471de89c3b6b5e086cf13
Kohta9521 <kohta0421kochi@gmail.com>
2023-01-28 01:30:34 +0900

    add udemy sample

--- tutorial/my-app/src/index.js
+++ tutorial/my-app/src/index.js
@@ -5,6 +5,14 @@ import './index.css';
 
 
 class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
+
+
     render() {
       return (
         <button className="square" onClick={function() { console.log('click'); }}>
---
+++ udemy/nextjs_hp/pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <h1 class="text-3xl font-bold underline text-orange-50">
+    Hello world!
+    </h1>
+  )
+}
