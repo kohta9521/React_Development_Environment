b6a5bcd846a7727d12faff3972f9067c8c88eb9c
Kohta9521 <kohta0421kochi@gmail.com>
2023-02-21 17:22:20 +0900

    はいrつの結合

--- feb/restudy/24/script.js
+++ feb/restudy/24/script.js
@@ -1,8 +1,26 @@
-// default
-const sayHello = (name = "ゲスト") => {
-  console.log(`こんにちは${name}さん!!`);
-};
+// スプレッド構文
+// 配列の展開
+const arr1 = [1, 2, 3, 4, 5];
+// console.log(arr1);
+// console.log(...arr1);
 
-sayHello();
+const sumFunc = (num1, num2) => console.log(num1 + num2);
+sumFunc(arr1[0], arr1[1]);
 
-// 変数null = undifined
+sumFunc(...arr1);
+
+const arr2 = [1, 2, 3, 4, 5];
+const [num1, num2, ...arr3] = arr2;
+console.log(num1);
+console.log(num2);
+console.log(arr3);
+
+// 配列のコピーや結合
+const arr4 = [10, 20];
+const arr5 = [30, 40];
+
+const arr6 = [...arr4];
+
+// 結合
+const arr7 = [...arr4, ...arr5];
+console.log(arr7);
