1724be3fe2597b854056d2220d07c48e00cdaa63
Kohta9521 <kohta0421kochi@gmail.com>
2023-02-21 23:15:17 +0900

    add incompleted area task add

--- feb/pureJsTodoApp/script.js
+++ feb/pureJsTodoApp/script.js
@@ -2,7 +2,21 @@ const onClickAdd = () => {
   // テキストボックスの値を取得・初期化
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
-  alert(inputText);
+
+  // divタグを生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // liタグの生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  // divタグの子要素に各要素の設定
+  div.appendChild(li);
+  console.log(div);
+
+  // 未完了のリストに追加
+  document.getElementById("incomplete-list").appendChild(div);
 };
 
 document
