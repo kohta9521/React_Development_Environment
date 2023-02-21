deaa7792806a98d79dfcd6adeea3cc6e73fcf02b
Kohta9521 <kohta0421kochi@gmail.com>
2023-02-21 17:30:45 +0900

    all file upload

---
+++ feb/restudy/22/script.js
@@ -0,0 +1,15 @@
+// const myProfile = {
+//   name: "hoge",
+//   age: 19,
+// };
+
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
+// console.log(message1);
+
+// const { name, age } = myProfile;
+
+// 配列で使用
+const myProfile2 = ["hoge", 45];
+
+const message2 = `名前は${myProfile2(0)}です。年齢は${myProfile2(1)}歳です。`;
+console.log(message2);
