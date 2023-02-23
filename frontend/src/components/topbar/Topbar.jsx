655a4ecbf9d37b8f1d0268958beea8cdf2e4c595
Kohta9521 <kohta0421kochi@gmail.com>
2023-02-23 18:34:49 +0900

    add material icons

--- frontend/src/components/topbar/Topbar.jsx
+++ frontend/src/components/topbar/Topbar.jsx
@@ -1,3 +1,4 @@
+import { Chat, Search, Notifications } from '@mui/icons-material';
 import React from 'react'
 
 export default function Topbar() {
@@ -11,6 +12,7 @@ export default function Topbar() {
       {/* CENTER AREA */}
       <div className='topbarCenter'>
         <div className='searchbar'>
+          <Search className='searchIcon' />
           <input type="text" className='searchInput' placeholder='探し物はなんですか？' />
         </div>
       </div>
@@ -18,10 +20,12 @@ export default function Topbar() {
       {/* RIGHT AREA */}
       <div className="topbarRight">
         <div className="topbarIconItem">
-          1
+          <Chat />
+          <span className='topbarIconBadge'>1</span>
         </div>
         <div className="topbarIconItem">
-          2
+          <Notifications />
+          <span className='topbarIconBadge'>2</span>
         </div>
         <img src="/assets/person/1.jpeg" className='topbarImg' alt="プロフィール写真" />
       </div>
