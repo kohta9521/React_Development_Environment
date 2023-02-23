e1e59b40e1af5657bcf2f8cba2d79e86df58501b
Kohta9521 <kohta0421kochi@gmail.com>
2023-02-23 19:02:11 +0900

    add all components

--- frontend/src/components/topbar/Topbar.jsx
+++ frontend/src/components/topbar/Topbar.jsx
@@ -21,15 +21,17 @@ export default function Topbar() {
 
       {/* RIGHT AREA */}
       <div className="topbarRight">
-        <div className="topbarIconItem">
-          <Chat />
-          <span className='topbarIconBadge'>1</span>
+        <div className='topbarItemIcons'>
+          <div className="topbarIconItem">
+            <Chat />
+            <span className='topbarIconBadge'>1</span>
+          </div>
+          <div className="topbarIconItem">
+            <Notifications />
+            <span className='topbarIconBadge'>2</span>
+          </div>
+          <img src="/assets/person/1.jpeg" className='topbarImg' alt="プロフィール写真" />
         </div>
-        <div className="topbarIconItem">
-          <Notifications />
-          <span className='topbarIconBadge'>2</span>
-        </div>
-        <img src="/assets/person/1.jpeg" className='topbarImg' alt="プロフィール写真" />
       </div>
     </div>
   );
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 
 import Topbar from '../../components/topbar/Topbar';
+import Sidebar from '../../components/sidebar/Sidebar';
+import Timeline from '../../components/timeline/Timeline';
+import Rightbar from '../../components/rightbar/Rightbar'
 
 export default function Home() {
   return (
     <>
       <Topbar />
-      {/* sidebar */}
+      <Sidebar />
+      <Timeline />
+      <Rightbar />
     </>
   );
 }
