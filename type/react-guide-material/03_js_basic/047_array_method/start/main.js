202e290e68a49f71e83b60d3ceb6773f550d4190
Kohta9521 <kohta0421kochi@gmail.com>
2023-02-23 11:25:46 +0900

    add filter and map func

--- type/react-guide-material/03_js_basic/047_array_method/start/main.js
+++ type/react-guide-material/03_js_basic/047_array_method/start/main.js
@@ -1,2 +1,14 @@
 const arry = [10, 20, 30, 40];
 const newArry = [];
+
+for (let i = 0; i < arry.length; i++) {
+  newArry.push(arry[i] * 2);
+}
+
+console.log(newArry);
+
+const newArry2 = arry.map((val) => {
+  return val * 2;
+});
+
+console.log(newArry2);
