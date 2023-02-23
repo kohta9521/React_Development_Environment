69e0c0266ed60243a4aef04b7d0fc851be6ddd7e
Kohta9521 <kohta0421kochi@gmail.com>
2023-02-23 19:20:43 +0900

    add sidebar html

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,7 +1,44 @@
+import { Bookmark, Home, MessageRounded, Person, Search, Settings } from '@mui/icons-material';
 import React from 'react'
 
 export default function Sidebar() {
   return (
-    <div>Sidebar</div>
+    <div className='sidebar'>
+      <div className='sidebarWrapper'>
+        <ul className="sidebarList">
+          <li className="sidebarListItem">
+            <Home  className='sidebarIcon'/>
+            <span className='sidebarListItemText'>Home</span>
+          </li>
+          <li className="sidebarListItem">
+            <Search  className='sidebarIcon'/>
+            <span className='sidebarListItemText'>Notifiactions</span>
+          </li>
+          <li className="sidebarListItem">
+            <MessageRounded  className='sidebarIcon'/>
+            <span className='sidebarListItemText'>Message</span>
+          </li>
+          <li className="sidebarListItem">
+            <Bookmark  className='sidebarIcon'/>
+            <span className='sidebarListItemText'>BookMark</span>
+          </li>
+          <li className="sidebarListItem">
+            <Person  className='sidebarIcon'/>
+            <span className='sidebarListItemText'>Profile</span>
+          </li>
+          <li className="sidebarListItem">
+            <Settings  className='sidebarIcon'/>
+            <span className='sidebarListItemText'>Settings</span>
+          </li>
+        </ul>
+        <hr className='sidebarHr' />
+        <ul className='sidebarFriendList'>
+          <li className="sidebarFriend">
+            <img src="/assets/person/2.jpeg" className='sidebarFriendImg' alt="" />
+            <span className='sidebarFriendName'>kohta</span>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 }
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import "./Home.css";
+
 import Topbar from '../../components/topbar/Topbar';
 import Sidebar from '../../components/sidebar/Sidebar';
 import Timeline from '../../components/timeline/Timeline';
@@ -9,9 +11,11 @@ export default function Home() {
   return (
     <>
       <Topbar />
-      <Sidebar />
-      <Timeline />
-      <Rightbar />
+      <div className='homeContainer'>
+        <Sidebar />
+        {/* <Timeline />
+        <Rightbar /> */}
+      </div>
     </>
   );
 }
