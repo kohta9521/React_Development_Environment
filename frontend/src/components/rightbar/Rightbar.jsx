a2da81693107ded9e529145db14f57b65dda05dd
Kohta9521 <kohta0421kochi@gmail.com>
2023-02-23 21:56:26 +0900

    add simple css stylring

--- frontend/src/components/rightbar/Rightbar.jsx
+++ frontend/src/components/rightbar/Rightbar.jsx
@@ -10,15 +10,22 @@ export default function Rightbar() {
           <img src="/assets/star.png" className='starImg' alt="" />
           <span className="eventText"><b>フォロワー限定</b>イベント開催中!!</span>
         </div>
-        <img src="/assets/ad.jpeg" className='' alt="" />
+        <img src="/assets/ad.jpeg" className='eventImg' alt="" />
         <h4 className='rightbarTitle'>オンラインの友達</h4>
         <ul className="rightbarFriendList">
           <li className='rightbarFriend'>
             <div className="rightbarProfileImgContainer">
-              <img src="/assets/person/3.jpeg" alt="" />
+              <img src="/assets/person/3.jpeg" className='rightbarProfileImg' alt="" />
               <span className="rightbarOnline"></span>
             </div>
             <span className="rightbarUserName">Suzuki</span>
+          </li>
+          <li className='rightbarFriend'>
+            <div className="rightbarProfileImgContainer">
+              <img src="/assets/person/4.jpeg" className='rightbarProfileImg' alt="" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUserName">Tanaka</span>
           </li>
         </ul>
       </div>
