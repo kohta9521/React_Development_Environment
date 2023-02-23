f14bc7d345100f0dbf856e2220ec7a04acf66a93
Kohta9521 <kohta0421kochi@gmail.com>
2023-02-23 18:22:56 +0900

    add topbar layout

--- frontend/src/components/topbar/Topbar.jsx
+++ frontend/src/components/topbar/Topbar.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 
-function Topbar() {
+export default function Topbar() {
   return (
-    <div>Topbar</div>
-  )
+    <div className='topbarContainer'>
+      <div className='topbarLeft'>
+        <span className='logo'>G Port</span>
+      </div>
+      <div className='topbarCenter'>
+        <div className='searchbar'>
+          <input type="text" className='searchInput' placeholder='探し物はなんですか？' />
+        </div>
+      </div>
+      <div className="topbarRight">
+        <div className="topbarIconItem">
+          1
+        </div>
+        <div className="topbarIconItem">
+          2
+        </div>
+        <img src="/assets/person/1.jpeg" className='topbarImg' alt="プロフィール写真" />
+      </div>
+    </div>
+  );
 }
-
-export default Topbar
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -6,6 +6,7 @@ export default function Home() {
   return (
     <>
       <Topbar />
+      {/* sidebar */}
     </>
   );
 }
