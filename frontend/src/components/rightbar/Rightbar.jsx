a71e8c3307904bb6780caafca2e9f0f238eab27a
Kohta9521 <kohta0421kochi@gmail.com>
2023-02-23 21:44:59 +0900

    rgithbar html

--- frontend/src/components/rightbar/Rightbar.jsx
+++ frontend/src/components/rightbar/Rightbar.jsx
@@ -5,6 +5,23 @@ import "./Rightbar.css";
 export default function Rightbar() {
   return (
     <div className="rightbar">
+      <div className="rightbarWrapper">
+        <div className="eventContainer">
+          <img src="/assets/star.png" className='starImg' alt="" />
+          <span className="eventText"><b>フォロワー限定</b>イベント開催中!!</span>
+        </div>
+        <img src="/assets/ad.jpeg" className='' alt="" />
+        <h4 className='rightbarTitle'>オンラインの友達</h4>
+        <ul className="rightbarFriendList">
+          <li className='rightbarFriend'>
+            <div className="rightbarProfileImgContainer">
+              <img src="/assets/person/3.jpeg" alt="" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUserName">Suzuki</span>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
