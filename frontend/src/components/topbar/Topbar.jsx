ac465bfd375bb8deb29e1fce49875a10f7397f97
Kohta9521 <kohta0421kochi@gmail.com>
2023-02-23 18:28:06 +0900

    layout and assets image

--- frontend/src/components/topbar/Topbar.jsx
+++ frontend/src/components/topbar/Topbar.jsx
@@ -3,14 +3,19 @@ import React from 'react'
 export default function Topbar() {
   return (
     <div className='topbarContainer'>
+      {/* LEFT AREA */}
       <div className='topbarLeft'>
         <span className='logo'>G Port</span>
       </div>
+
+      {/* CENTER AREA */}
       <div className='topbarCenter'>
         <div className='searchbar'>
           <input type="text" className='searchInput' placeholder='探し物はなんですか？' />
         </div>
       </div>
+
+      {/* RIGHT AREA */}
       <div className="topbarRight">
         <div className="topbarIconItem">
           1
