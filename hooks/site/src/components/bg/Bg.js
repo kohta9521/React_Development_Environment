f47b75f2677c97ccf6d3f3414611325ef7079cd7
Kohta9521 <kohta0421kochi@gmail.com>
2023-03-04 16:50:59 +0900

    add bg coor change usestate

--- hooks/site/src/components/bg/Bg.js
+++ hooks/site/src/components/bg/Bg.js
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
 // css import
 import "./Bg.css";
 
 function Bg() {
+  const [isDark, setDark] = useState(false);
+
+  const bgColor = isDark ? "black" : "white";
   return (
     <>
-      <div className="content">
-        <p>The Background Color is white.</p>
+      <div className={`content bg-${bgColor}`}>
+        <p>The Background Color is {bgColor}.</p>
       </div>
     </>
   );
 }
 
 ReactDOM.render(<Bg />, document.getElementById("root"));
+export default Bg;
