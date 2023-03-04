3024221a60f8cd216b2c5e4bccb5a0b2978de50f
Kohta9521 <kohta0421kochi@gmail.com>
2023-03-04 17:28:18 +0900

    add all files

--- hooks/site/src/App.js
+++ hooks/site/src/App.js
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import "./App.css";
 
+import Bg from "./components/bg/Bg";
+import Form from "./components/form/Form";
+import Form2 from "./components/form/Form2";
+
 // 関数コンポーネント
 const App = () => {
   // useStateの呼び出し
@@ -12,6 +16,11 @@ const App = () => {
         <p>{count}回クリックしました。</p>
         <button onClick={() => setCount(count + 1)}>クリック</button>
       </div>
+
+      <Bg />
+      <Form />
+
+      <Form2 />
     </>
   );
 };
