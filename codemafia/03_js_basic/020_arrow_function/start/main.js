8c78db74fb582f5d854137cb4f76b417cbe1a887
Kohta9521 <kohta0421kochi@gmail.com>
2023-03-05 20:31:46 +0900

    add arrow funciton attentio n object

--- codemafia/03_js_basic/020_arrow_function/start/main.js
+++ codemafia/03_js_basic/020_arrow_function/start/main.js
@@ -1,19 +1,28 @@
-function fn(number) {
-  return number * 2;
-}
+// function fn(number) {
+//   return number * 2;
+// }
 
-const fn = function (number) {
+// const fn = function (number) {
+//   return number * 2;
+// };
+
+// console.log(fn(2));
+
+// console.log(2);
+
+// const fn = (number) => {
+//   return number * 2;
+// };
+
+// console.log(2);
+
+// const FnArrow = (number) => number * 2;
+
+// 複数行で出力する場合
+const fnArrow = (number) => {
+  console.log(number);
   return number * 2;
 };
 
-console.log(fn(2));
-
-console.log(2);
-
-const fn = (number) => {
-  return number * 2;
-};
-
-console.log(2);
-
-const FnArrow = (number) => number * 2;
+const fnArrowObj = (number) => ({ result: number * 2 });
+console.log(fnArrowObj(2));
