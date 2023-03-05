4a76957979f77d94e11019caa31844a9842f82dc
Kohta9521 <kohta0421kochi@gmail.com>
2023-03-05 20:26:16 +0900

    new arrow function

--- codemafia/03_js_basic/020_arrow_function/start/main.js
+++ codemafia/03_js_basic/020_arrow_function/start/main.js
@@ -1,3 +1,19 @@
 function fn(number) {
   return number * 2;
 }
+
+const fn = function (number) {
+  return number * 2;
+};
+
+console.log(fn(2));
+
+console.log(2);
+
+const fn = (number) => {
+  return number * 2;
+};
+
+console.log(2);
+
+const FnArrow = (number) => number * 2;
