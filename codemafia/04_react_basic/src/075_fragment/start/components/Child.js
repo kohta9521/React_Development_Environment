7acdd6a6450cc27f1c194e586aca172078be1529
Kohta9521 <kohta0421kochi@gmail.com>
2023-03-06 10:02:39 +0900

    add fragment

--- codemafia/04_react_basic/src/075_fragment/start/components/Child.js
+++ codemafia/04_react_basic/src/075_fragment/start/components/Child.js
@@ -1,10 +1,21 @@
 import "./Child.css";
 
+import React from "react";
+
 const Child = () => {
   return (
-    <div className="component">
-      <h3>Hello Component</h3>
-    </div>
+    <React.Fragment>
+      <div className="component">
+        <h3>Hello Component</h3>
+      </div>
+      <h3>Hello fragment</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste magnam
+        explicabo optio enim ut laborum adipisci, animi recusandae commodi,
+        sapiente voluptate culpa consequatur voluptatem dolores quae labore est,
+        odio sint.
+      </p>
+    </React.Fragment>
   );
 };
 
