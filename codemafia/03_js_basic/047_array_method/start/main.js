4aacf9563687d21adf6b1f4855dc1c1f0574529e
Kohta9521 <kohta0421kochi@gmail.com>
2023-03-09 07:25:11 +0900

    add map and filter doule

--- codemafia/03_js_basic/047_array_method/start/main.js
+++ codemafia/03_js_basic/047_array_method/start/main.js
@@ -1,21 +1,14 @@
 const arry = [10, 20, 30, 40];
 const newArry = [];
 
-for (let i = 0; i < arry.length; i++) {
+for (let i = 0; i < arry.ength; i++) {
   const val = arry[i] * 2;
   if (val > 50) {
-    newArry.push(arry[i]);
+    newArry.push(arry[i] * 2);
   }
-  newArry.push(arry[i] * 2);
 }
 
 console.log(newArry);
 
-const newArry2 = arry.map((val, i, arry) => {
-  console.log(val, i, arry);
-  return val * 3;
-});
-
-const newArry3 = newArry.filter((val) => val > 50);
-console.log(newArry3);
+const newArry2 = arry.map((val) => val * 2).filter((val) => val > 50);
 console.log(newArry2);
