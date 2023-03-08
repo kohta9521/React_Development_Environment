43fc53503afe2e9752bf61295a6bb855be2048cb
Kohta9521 <kohta0421kochi@gmail.com>
2023-03-09 07:45:00 +0900

    レストパラメーター

--- codemafia/03_js_basic/060_spread_rest/start/main.js
+++ codemafia/03_js_basic/060_spread_rest/start/main.js
@@ -1,25 +1,29 @@
-const nums = [3, 1, 4, 1, 5, 10, 2, 6];
+// const nums = [3, 1, 4, 1, 5, 10, 2, 6];
 
-const result = Math.max(3, 1, 4, 1, 5, 10, 2, 6);
-console.log(result);
+// const result = Math.max(...nums);
+// console.log(result);
 
+// let arr1 = [1, 2, 3];
+// let arr2 = [4, 5, 6];
+// let newArr = [...arr1, ...arr2, 10];
+// let newArr1 = arr1;
+// newArr1.push(4);
+// console.log(newArr);
 
-let arr1 = [1, 2, 3];
-let arr2 = [4, 5, 6];
-let newArr = [...arr1];
-
-console.log(newArr2);
+// console.log(newArr1);
 
 const obj = {
   name: "Tom",
   age: 22,
 };
 const newObj = { ...obj };
+newObj.name = "John";
 
 console.log(newObj);
 
+// レストパラメーター
 const restA = (...argA) => console.log(argA);
-restA(1, 3, 4)
+restA(1, 3, 4, 5);
 
 const restB = (n, ...argB) => console.log(argB);
-restB(1, 3, 4)
+restB(1, 3, 4);
