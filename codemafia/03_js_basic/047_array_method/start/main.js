261a1800a95d3a49f9a04cfe94e2f6b28f6a764e
Kohta9521 <kohta0421kochi@gmail.com>
2023-03-09 07:14:08 +0900

    add map ad filter

--- codemafia/03_js_basic/047_array_method/start/main.js
+++ codemafia/03_js_basic/047_array_method/start/main.js
@@ -1,2 +1,21 @@
 const arry = [10, 20, 30, 40];
 const newArry = [];
+
+for (let i = 0; i < arry.length; i++) {
+  const val = arry[i] * 2;
+  if (val > 50) {
+    newArry.push(arry[i]);
+  }
+  newArry.push(arry[i] * 2);
+}
+
+console.log(newArry);
+
+const newArry2 = arry.map((val, i, arry) => {
+  console.log(val, i, arry);
+  return val * 3;
+});
+
+const newArry3 = newArry.filter((val) => val > 50);
+console.log(newArry3);
+console.log(newArry2);
