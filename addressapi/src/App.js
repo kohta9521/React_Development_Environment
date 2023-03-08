5f29ffe487297ea343f34113b1261c7c2c403404
Kohta9521 <kohta0421kochi@gmail.com>
2023-03-08 20:34:09 +0900

    add address api status json

--- addressapi/src/App.js
+++ addressapi/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Address from "./components/Address";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>業務で使用する住所検索APIの実装練習</h1>
+      <Address />
     </div>
   );
 }
--- addressapi/src/components/Address.jsx
+++ addressapi/src/components/Address.jsx
@@ -1,9 +1,46 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect } from 'react';
 import axios from 'axios';
 import './Address.css';
 
 export default function Address() {
   const [ zip, setZip ] = useState("");
+  const [query, setQuery ] = useState("");
+  const [ resultTxt, setResultTxt ] = useState("");
+
+  useEffect(() => {
+    const fetchData = () => {
+      console.log(" データを取得します");
+      console.log(query);
+
+      axios.get(`https://zipcloud.ibsnet.co.jp/api/search?zipcode=${query}`).then((res) => {
+        console.log(res);
+        // APIが作動していない場合のエラー
+        if (res.status !== 200) {
+          throw new Error('APIがうまく作動していないようです');
+        } else {
+          // 郵便番号の桁数が不正の場合のメッセージ
+        if (res.data.message) {
+          setResultTxt(res.data.message);
+          return;
+        }
+
+        // 郵便番号が存在しない場合のメッセージ
+        if (res.data,result == null) {
+          setResultTxt('郵便番号が見つかりませんでした。');
+          return;
+        }
+
+        // 取得した住所を格納
+        let getAddress = res.data.result[0];
+
+        setResultTxt(
+          `〒${getAddress.zipcode}\n${getAddress.address1}$getAddress.address2${getAddress.address3}`
+        );
+        }
+      })
+      .catch(err)=> setResultTxt(`データがうまく取得できませんでした。${err}`);
+    };
+  })
 
   return (
     <div>
