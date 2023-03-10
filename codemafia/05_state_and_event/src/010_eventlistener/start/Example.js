54fe084b9c3e8b9f5f1953dc9daadb9a00d4d88b
Kohta9521 <kohta0421kochi@gmail.com>
2023-03-10 16:38:14 +0900

    add clickHandler

--- codemafia/05_state_and_event/src/010_eventlistener/start/Example.js
+++ codemafia/05_state_and_event/src/010_eventlistener/start/Example.js
@@ -2,15 +2,10 @@ const Example = () => {
   const clickHandler = () => {
     alert("ボタンがクリックされました");
   };
-
-  const clickHandler2 = () => {
-    console.log("ボタンがクリックされました");
-  };
-
   return (
     <>
       <button onClick={clickHandler}>クリックしてね</button>
-      <button onClick={clickHandler2}>クリックしてね</button>
+      <button>クリックしてね</button>
     </>
   );
 };
