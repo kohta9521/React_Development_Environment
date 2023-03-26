366f718cb3eed1e58fc7b059cfd25efa1ad7da90
Kohta9521 <kohta0421kochi@gmail.com>
2023-03-26 14:05:04 +0900

    add node modules and

--- lastReact/03_js_basic/020_arrow_function/start/main.js
+++ lastReact/03_js_basic/020_arrow_function/start/main.js
@@ -1,3 +1,11 @@
-function fn(number) {
+const fn = (number) => {
   return number * 2;
-}
+};
+
+console.log(fn(2));
+
+const fnArroq = (number) => {
+  return number * 2;
+};
+
+console.log(fnArroq(2));
