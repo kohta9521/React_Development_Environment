c620c263d392bb10d7b1bbab03546aaaaaf35191
Kohta9521 <kohta0421kochi@gmail.com>
2023-03-26 18:22:40 +0900

    add list and child components

--- lastReact/04_react_basic/src/070_component_nest/start/Example.js
+++ lastReact/04_react_basic/src/070_component_nest/start/Example.js
@@ -1,9 +1,9 @@
-import "./Example.css";
+import Child from "./components/Child";
 
 const Example = () => {
   return (
-    <div className="component">
-      <h3>Hello Component</h3>
+    <div className="components">
+      <Child />
     </div>
   );
 };
