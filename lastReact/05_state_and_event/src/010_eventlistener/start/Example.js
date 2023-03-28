fc100c868c0d725728f4aeeb728dff0ba31a8aa8
Kohta9521 <kohta0421kochi@gmail.com>
2023-03-28 21:03:58 +0900

    complete section4 and section5 node modeulew

--- lastReact/05_state_and_event/src/010_eventlistener/start/Example.js
+++ lastReact/05_state_and_event/src/010_eventlistener/start/Example.js
@@ -1,6 +1,13 @@
 const Example = () => {
+  const clickHandler = () => {
+    alert("クリックされました");
+  };
+
   return (
-    <></>
+    <>
+      <button onClick={clickHandler}>クリックしてね</button>
+      <button>クリックしてね</button>
+    </>
   );
 };
 
