92f0e8554a26b7c943c9885d58482751fccedb54
Kohta9521 <kohta0421kochi@gmail.com>
2023-04-02 18:35:23 +0900

    add all files

--- lastReact/06_control_and_form/src/010_list_components/start/Example.js
+++ lastReact/06_control_and_form/src/010_list_components/start/Example.js
@@ -1,13 +1,21 @@
-
 const animals = ["Dog", "Cat", "Rat"];
-
+const animalList = [];
 const Example = () => {
+  for (const animal of animals) {
+    animalList.push(<li>Hello : {animal}</li>);
+  }
+
+  const helloAnimals = animals.map((animal) => <li>Hello, {animal} 2</li>);
 
   return (
     <>
       <h3>配列の操作</h3>
       <ul>
-        
+        {/* <li>{animals[0]}</li>
+        <li>{animals[1]}</li>
+        <li>{animals[2]}</li> */}
+        {animalList}
+        {helloAnimals}
       </ul>
     </>
   );
