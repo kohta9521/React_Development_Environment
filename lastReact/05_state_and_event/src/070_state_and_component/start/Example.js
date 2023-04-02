146390805c0d6048036318970aed9d184cb60f2a
Kohta9521 <kohta0421kochi@gmail.com>
2023-04-02 18:03:55 +0900

    add today first comit 2023/04/02

--- lastReact/05_state_and_event/src/070_state_and_component/start/Example.js
+++ lastReact/05_state_and_event/src/070_state_and_component/start/Example.js
@@ -1,8 +1,17 @@
 import { useState } from "react";
 
 const Example = () => {
+  const [toggle, setToggle] = useState(true);
+  const toggleComponent = () => {
+    setToggle((prev) => !prev);
+  };
+  return <>{toggle ? <Count title="A" /> : <Count title="B" />}</>;
+};
+
+const Count = ({ title }) => {
   const [count, setCount] = useState(0);
   const countUp = () => {
+    <button onClick={toggleComponent}>toggle</button>;
     setCount((prevstate) => prevstate + 1);
   };
   const countDown = () => {
@@ -10,7 +19,9 @@ const Example = () => {
   };
   return (
     <>
-      <h3>カウント: {count}</h3>
+      <h3>
+        {title}: {count}
+      </h3>
       <button onClick={countUp}>+</button>
       <button onClick={countDown}>-</button>
     </>
